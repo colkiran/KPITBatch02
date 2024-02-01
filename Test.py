635081e4157498c55332f12b058cf3638508dbca
Kiran <colkiran@gmail.com>
2024-02-01 16:55:43 +0530

    commit

--- Test.py
+++ Test.py
@@ -1,13 +1,42 @@
-matrix=[]
-row=int(input("Enter the rows:"))
-col=int(input("Enter the col"))
-for i in range(row):
-    row=[]
-    for j in range(col):
-        ele=input(f"input the element({i+1},{j+1}):")
-        row.append(ele)
+# Q3.  Transpose of matrix, matrix can be any shape for example
+#
+# input
+#
+# 1  2  3		1  2  3
+# 4  5  6		4  5  6
+# 7  8  9
+#
+# output
+#
+# 1  4  7 	1  4
+# 2  5  8		2  5
+# 3  6  9		3  6
+
+
+def transpose(matrix):
+    transposed = []
+    for i in range(len(matrix[0])):
+        transposed_row = []
+        for row in matrix:
+            transposed_row.append(row[i])
+        transposed.append(transposed_row)
+    return transposed
+# here we are taking input
+rows = int(input("Enter the number of rows: ")) # row
+cols = int(input("Enter the number of columns: ")) # coloum
+
+matrix = [] # making  empty matrix
+
+print("Enter the elements of the matrix:")
+for i in range(rows):
+    row = list(map(int, input().split()))
     matrix.append(row)
 
-transposeofmatrix=[[matrix[j][i] for j in range (len(matrix))] for i in range(len(matrix[0]))]
-print(matrix)
-print(transposeofmatrix)
+print("Original matrix:")
+for row in matrix:
+    print(row)
+
+transpose= transpose(matrix)
+print("Transposed matrix:")
+for row in transpose:
+    print(row)
